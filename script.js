9fabfc2394c702790ec884b03148a2eda350a71d
Anirban Dutta <68235038+dutta-anirban@users.noreply.github.com>
2021-05-06 03:56:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,10 +30,11 @@ WARNING!!! WARNING!!!
 (function () {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
+    var firstLetter = (names[i].charAt(0)).toLowerCase();
     if (firstLetter === 'j') {
       byeSpeaker(names[i]);
-    } else {
+    } 
+    else {
     helloSpeaker(names[i]);
     }
   }
